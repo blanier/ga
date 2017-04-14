4071b9d2b623cbd02a24d1df57b39b2f8dd94d61
Brian Lanier <brian@lanier.org>
2017-04-13 23:17:08 -0700

    refactor and integrate firebase

--- main.js
+++ main.js
@@ -1,30 +1,130 @@
-var parameters = {
-  interpreter: {
-    memory_size: 1000,
-    instruction_count: 5000
-  },
+/*global firebase firebaseConfig c3 d3:true*/
+/*eslint no-alert:off*/
+/*eslint no-console:off*/
 
-  randomness: {
-    seed: ""
-  },
 
-  generation: {
-    size: 100,
-    age_threshold: 10,
-    mutation_threshold: .5,
-    check_for_dups: true
-  },
+// Initialze GA
+function GA() {
+  this.checkSetup();
+  this.initFirebase();
 
-  goal: {
-    target: "hello world",
-    input: ""
+  this.startButton = document.getElementById("start_thread");
+  this.startButton.addEventListener('click', this.startWorker.bind(this));
+
+  this.stopButton = document.getElementById("stop_thread");
+  this.stopButton.addEventListener('click', this.stopWorker.bind(this));
+  this.stopWorker();
+
+  this.configDisplay = document.getElementById("config");
+  this.configsSelector = document.getElementById("config_selector");
+  this.configsSelector.addEventListener('change', this.onConfigChange.bind(this));
+  this.configsRef = this.database.ref("configs");
+  this.loadConfigs();
+}
+
+// Loads chat messages history and listens for upcoming ones.
+GA.prototype.loadConfigs = function() {
+  // Make sure we remove all previous listeners.
+  this.configsRef.off();
+
+  // Loads the known configs and listen for new ones.
+  var getConfig = function(data) {
+    var first = this.configsSelector.selectedIndex == -1;
+    var val = data.val();
+    var o = this.configsSelector.options[data.key];
+    if (!o) {
+      o = document.createElement("option");
+    }
+    o.text = val.metadata.name;
+    o.value = JSON.stringify(val);
+    o.id = data.key;
+    this.configsSelector.add(o);
+    if (first) { this.onConfigChange() }
+  }.bind(this);
+
+  // Listen for changes
+  this.configsRef.on('child_added', getConfig);
+  this.configsRef.on('child_changed', getConfig);
+};
+
+GA.prototype.onConfigChange = function() {
+  var v = this.configsSelector[this.configsSelector.selectedIndex].value;
+  parameters = JSON.parse(v);
+  this.configDisplay.innerHTML = `<pre> ${JSON.stringify(parameters,null,4)}</pre>`;
+}
+
+GA.prototype.resetDisplayState = function() {
+  columns = [ ['x'], ['fittest_ever'], ['fittest_now'], ['gps'], ['age']];
+  fittest_objects = {}
+  fittest = Number.MAX_VALUE;
+  fittest_now = { fitness: Number.MAX_VALUE};
+  fittest_ever = { fitness: Number.MAX_VALUE};
+  chart.load({columns:columns});
+}
+
+GA.prototype.startWorker = function() {
+  if (this.worker) { this.stopWorker() }
+
+  this.resetDisplayState();
+  this.startButton.style.display = "none";
+  this.stopButton.style.display = "inline";
+
+  this.worker = new Worker("hello.js");
+  this.worker.addEventListener('message', function(e) {
+    if (e.data.type === "generation") {
+        updateFittest(e.data.data);
+        displayEntities(e.data.data);
+    } else if ( e.data.type === "stats" ) {
+        displayStats(e.data.data);
+    } else {
+        console.log(e.data);
+    }
+  }, false);
+  this.worker.postMessage({cmd: "start", parameters: JSON.stringify(parameters)});
+}
+
+GA.prototype.stopWorker = function() {
+  if (this.worker) { this.worker.terminate(); }
+  this.worker = undefined;
+  this.startButton.style.display = "inline";
+  this.stopButton.style.display = "none";
+}
+
+// Sets up shortcuts to Firebase features and initiate firebase auth.
+GA.prototype.initFirebase = function() {
+  // Shortcuts to Firebase SDK features.
+  this.auth = firebase.auth();
+  this.database = firebase.database();
+  this.storage = firebase.storage();
+  // Initiates Firebase auth and listen to auth state changes.
+  // this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+};
+
+// Checks that the Firebase SDK has been correctly setup and configured.
+GA.prototype.checkSetup = function() {
+  if (!window.firebase || !(firebase.app instanceof Function) || !window.config) {
+    window.alert('You have not configured and imported the Firebase SDK. ' +
+        'Make sure you go through the codelab setup instructions.');
+  } else if (firebaseConfig.storageBucket === '') {
+    window.alert('Your Cloud Storage bucket has not been enabled. Sorry about that. This is ' +
+        'actually a Firebase bug that occurs rarely. ' +
+        'Please go and re-generate the Firebase initialisation snippet (step 4 of the codelab) ' +
+        'and make sure the storageBucket attribute is not empty. ' +
+        'You may also need to visit the Storage tab and paste the name of your bucket which is ' +
+        'displayed there.');
   }
 };
 
-var worker = undefined;
+var parameters = {};
+var columns = [ ['x'], ['fittest_ever'], ['fittest_now'], ['gps'], ['age']];
 var chart;
+var fittest_objects = {}
+var fittest = Number.MAX_VALUE;
+var fittest_now = { fitness: Number.MAX_VALUE };
+var fittest_ever = { fitness: Number.MAX_VALUE };
 
 window.onload = function() {
+    window.GA = new GA();
 
     // handle randomness initialization
     // if (parameters.randomness.seed && paramaters.randomnedd.seed != "") {
@@ -76,8 +176,6 @@ window.onload = function() {
           }
         }
     });
-    toggleWorker();
-    document.getElementById("config").innerHTML = `<pre> ${JSON.stringify(parameters,null,4)}</pre>`;
 };
 
 function displayEntities(e) {
@@ -101,9 +199,6 @@ function displayEntities(e) {
     document.getElementById("Generation").innerHTML = t;
 }
 
-var columns = [ ['x'], ['fittest_ever'], ['fittest_now'], ['gps'], ['age']];
-var fittest_objects = {}
-
 function safeDisplay(s) {
     var rv = encodeURI(s);
     rv = rv.replace(/%20/g," ");
@@ -114,10 +209,6 @@ function roundedDisplay(f) {
     f = Math.round(f * 1000)/1000;
     return f;
 }
-
-var fittest = Number.MAX_VALUE;
-var fittest_now;
-var fittest_ever;
 
 function displayStats(s) {
     let gps = Math.round(s.entity_count/s.elapsed*1000);
@@ -130,8 +221,8 @@ function displayStats(s) {
     var now = new Date(s.now);
 
     fittest_objects[now] = fittest_ever;
-    columns[0].push(now);
 
+    columns[0].push(now);
     let log_scale = true
     if (log_scale) {
       columns[1].push(Math.log(fittest_ever.fitness) / Math.LN10);
@@ -147,35 +238,14 @@ function displayStats(s) {
 
 function updateFittest(e) {
     fittest_now = JSON.parse(JSON.stringify(e[0]));
-    if (e[0].fitness < fittest) {
+    if (fittest_now.fitness < fittest_ever.fitness) {
         fittest_ever = fittest_now;
-        fittest = e[0].fitness;
+        fittest = fittest_ever.fitness;
         document.title = parameters.goal.target + " : " + fittest;
     }
 
     // stop if we match
     if (true && e[0].output == parameters.goal.target) {
-        worker.postMessage({cmd: "stats and die" });
-        // toggleWorker();
+        window.GA.worker.postMessage({cmd: "stats and die" });
     }
-}
-
-function toggleWorker() {
-  if (worker) {
-    worker.terminate();
-    worker = undefined;
-  } else {
-    worker = new Worker("hello.js");
-    worker.addEventListener('message', function(e) {
-      if (e.data.type === "generation") {
-          updateFittest(e.data.data);
-          displayEntities(e.data.data);
-      } else if ( e.data.type === "stats" ) {
-          displayStats(e.data.data);
-      } else {
-          console.log(e.data);
-      }
-    }, false);
-    worker.postMessage({cmd: "start", parameters: JSON.stringify(parameters)});
-  }
 }
